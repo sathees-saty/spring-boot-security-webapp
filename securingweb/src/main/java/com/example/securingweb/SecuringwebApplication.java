9a9a47733eb37394d6d8c0e1ad57343e9f2afd23
Sathees Kumar K <65384711+sathees-saty@users.noreply.github.com>
2024-03-31 09:59:54 +0530

    Add files via upload

---
+++ securingweb/src/main/java/com/example/securingweb/SecuringwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringwebApplication.class, args);
+	}
+
+}
---
+++ securingweb/src/test/java/com/example/securingweb/SecuringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
